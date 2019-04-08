c5acff4b4b3af956ba7cfff9110de328b94cea1e
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:38:56 +0000

    Mon Apr 08 14:38:56 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,7 +2,8 @@
 class FizzBuzz
   
   def fizz_buzz(number)
-    return "fizz buzz deluxe" if (((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1)))%2 == 0 else return "fizz buzz fake deluxe"
+    return "fizz buzz deluxe" if (((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1))) % 2 == 0
+    return "fizz buzz fake deluxe" if (((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1))) % 2 != 0
     return "fizz deluxe" if ((number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1))
     return "buzz deluxe" if ((number % 5 == 0 || number.to_s.include?('5')) && (number > 10 && number.to_s.chars.uniq.length == 1))
     return "fake deluxe" if (number > 10 && number.to_s.chars.uniq.length == 1 && number % 2 !=0)
@@ -14,6 +15,7 @@ class FizzBuzz
   end
 
 end
+
 
 
 
