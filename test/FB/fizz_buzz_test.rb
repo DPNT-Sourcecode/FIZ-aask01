4767df7d9c8ae0b264e0fb0573511daa6676cc81
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 16:05:46 +0000

    Mon Apr 08 16:05:46 UTC 2019

--- test/FB/fizz_buzz_test.rb
+++ test/FB/fizz_buzz_test.rb
@@ -10,8 +10,8 @@ class ClientTest < Minitest::Test
   def test_fiz
     assert_equal "fizz buzz", FizzBuzz.new.fizz_buzz(546),   'App returns a string'
     assert_equal "fizz buzz", FizzBuzz.new.fizz_buzz(653),   'App returns a string'
-    assert_equal "fizz", FizzBuzz.new.fizz_buzz(3),   'App returns a string'
-    assert_equal "buzz", FizzBuzz.new.fizz_buzz(5),   'App returns a string'
+    assert_equal "fizz fake deluxe", FizzBuzz.new.fizz_buzz(3),   'App returns a string'
+    assert_equal "buzz fake deluxe", FizzBuzz.new.fizz_buzz(5),   'App returns a string'
     assert_equal 1, FizzBuzz.new.fizz_buzz(1),   'App returns an integer'
     assert_equal "deluxe", FizzBuzz.new.fizz_buzz(22),   'App returns a string'
     assert_equal "fizz buzz fake deluxe", FizzBuzz.new.fizz_buzz(555),   'App returns a string'
@@ -24,6 +24,7 @@ class ClientTest < Minitest::Test
   
 
 end
+
 
 
 
