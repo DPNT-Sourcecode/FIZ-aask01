416dc91caf8d1c82f1d281eaec74661d182e5e19
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 11:12:59 +0000

    Mon Apr 08 11:12:59 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,11 +2,12 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    return "fizz buzz" if (number % 3 == 0 && number % 5 ==0)
+    return "fizz buzz" if number % 15 == 0
     return "fizz" if number % 3 == 0
     return "buzz" if number % 5 == 0
     return number 
   end
 
 end
+
 
--- test/FB/fizz_buzz_test.rb
+++ test/FB/fizz_buzz_test.rb
@@ -8,11 +8,14 @@ require_solution 'FIZ'
 class ClientTest < Minitest::Test
 
   def test_fiz
+    assert_equal "fizz buzz", FizzBuzz.new.fizz_buzz(15),   'App returns a string'
     assert_equal "fizz", FizzBuzz.new.fizz_buzz(3),   'App returns a string'
     assert_equal "buzz", FizzBuzz.new.fizz_buzz(5),   'App returns a string'
+    assert_equal 4, FizzBuzz.new.fizz_buzz(4),   'App returns an integer'
 
   end
   
 
 end
+
 
