b6144dfb0341bc97dbf5cd85d74751f122aa021c
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:22:59 +0000

    Mon Apr 08 13:22:59 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,13 +1,17 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
-  
+  def deluxe(number)
+    @deluxe = 
+  end
+
   def fizz_buzz(number)
     return "fizz buzz" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')))
     return "fizz" if (number % 3 == 0 || number.to_s.chars.include?('3')) 
     return "buzz" if (number % 5 == 0 || number.to_s.chars.include?('5'))
     return number
-    return "fizz buzz deluxe" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')))
+    return "fizz buzz deluxe" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number)
   end
 
 end
+
 
