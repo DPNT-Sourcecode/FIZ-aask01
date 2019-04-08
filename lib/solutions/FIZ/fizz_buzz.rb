c78305f15439aa13e3143f3c70d8b0b7c540069a
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:40:59 +0000

    Mon Apr 08 13:40:59 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,22 +1,13 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
-  def deluxe
-    
-  end
-
+  
   def fizz_buzz(number)
     return "fizz buzz" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')))
     return "fizz" if (number % 3 == 0 || number.to_s.chars.include?('3')) 
     return "buzz" if (number % 5 == 0 || number.to_s.chars.include?('5'))
     return number
-    return "delux" if (number > 10 && number.length)
-    return "fizz buzz deluxe" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number)
+    return "delux" if (number > 10 && number.to_s.chars.uniq.length == 1)
+    return "fizz buzz deluxe" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1)
   end
 
 end
-
-
-
-
-
-
--- test/FB/fizz_buzz_test.rb
+++ test/FB/fizz_buzz_test.rb
@@ -19,5 +19,3 @@ class ClientTest < Minitest::Test
   
 
 end
-
-
