128ff3b16dab3a1b40162fb723ce2048d75fea53
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:44:56 +0000

    Mon Apr 08 14:44:56 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,8 +2,8 @@
 class FizzBuzz
   
   def fizz_buzz(number)
-    return "fizz buzz deluxe" if (((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1))) % 2 == 0)
-    return "fizz buzz fake deluxe" if (((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1))) % 2 != 0)
+    return "fizz buzz deluxe" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1))) % 2 == 0
+    return "fizz buzz fake deluxe" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1))) % 2 != 0
     return "fizz deluxe" if ((number % 3 == 0 || number.to_s.include?('3')) && (number > 10 && number.to_s.chars.uniq.length == 1))
     return "buzz deluxe" if ((number % 5 == 0 || number.to_s.include?('5')) && (number > 10 && number.to_s.chars.uniq.length == 1))
     return "fake deluxe" if (number > 10 && number.to_s.chars.uniq.length == 1 && number % 2 !=0)
@@ -15,3 +15,4 @@ class FizzBuzz
   end
 
 end
+
--- test/FB/fizz_buzz_test.rb
+++ test/FB/fizz_buzz_test.rb
@@ -14,7 +14,7 @@ class ClientTest < Minitest::Test
     assert_equal "buzz", FizzBuzz.new.fizz_buzz(5),   'App returns a string'
     assert_equal 1, FizzBuzz.new.fizz_buzz(1),   'App returns an integer'
     assert_equal "deluxe", FizzBuzz.new.fizz_buzz(22),   'App returns a string'
-    # assert_equal "fizz buzz deluxe", FizzBuzz.new.fizz_buzz(555),   'App returns a string'
+    assert_equal "fizz buzz fake deluxe", FizzBuzz.new.fizz_buzz(555),   'App returns a string'
     assert_equal "fizz deluxe", FizzBuzz.new.fizz_buzz(33),   'App returns a string'
     assert_equal "fake deluxe", FizzBuzz.new.fizz_buzz(11),   'App returns a string'
 
@@ -22,3 +22,4 @@ class ClientTest < Minitest::Test
   
 
 end
+
