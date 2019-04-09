902cb7d72717c0896078f9b023047aca6280c8b0
sbx_user1051 <sbx_user1051@169.254.160.173>
2019-04-09 13:00:20 +0000

    Tue Apr 09 13:00:20 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,43 +1,6 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
   
-  def fizz_buzz(number) 
-    return "fizz buzz" if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')))
-
-    if (number % 15 == 0) && ((number % 5 == 0 && number.to_s.include?('5')) || (number % 3 == 0 && number.to_s.include?('3')))
-      if  number % 2 != 0
-      return "fizz buzz fake deluxe"
-      else return "fizz buzz  deluxe"
-      end
-    end
-
-    if (number % 3 == 0 && number.to_s.include?('3')) 
-      if number % 2 == 0
-        return "fizz deluxe"
-      else  return "fizz fake deluxe"
-      end
-    end
-
-
-    if (number % 5 == 0 && number.to_s.include?('5'))
-      if number % 2 != 0
-        return "buzz fake deluxe"
-      else return "buzz deluxe"
-      end
-    end
-    
-   
-    if (number % 5 == 0 && number.to_s.include?('5') || number % 3 == 0 && number.to_s.include?('3'))
-      if number % 2 != 0
-        return "fake deluxe"
-      else return "deluxe"
-      end
-    end
-   
-    return "fizz" if (number % 3 == 0 || number.to_s.chars.include?('3')) 
-    return "buzz" if (number % 5 == 0 || number.to_s.chars.include?('5'))
-
-    return number
-  end
-
+  return number 
 end
+
