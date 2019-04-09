e8f0c5f5eaf7cf852985a4cf4893848398a2ed21
sbx_user1051 <sbx_user1051@169.254.196.241>
2019-04-09 11:22:34 +0000

    Tue Apr 09 11:22:34 UTC 2019

--- test/FB/fizz_buzz_test.rb
+++ test/FB/fizz_buzz_test.rb
@@ -13,9 +13,12 @@ class ClientTest < Minitest::Test
     assert_equal "fizz deluxe", FizzBuzz.new.fizz_buzz(36),   'App returns a string'
     assert_equal "fizz fake deluxe", FizzBuzz.new.fizz_buzz(3),   'App returns a string'
     assert_equal "buzz fake deluxe", FizzBuzz.new.fizz_buzz(55),   'App returns a string'
+    assert_equal "buzz deluxe", FizzBuzz.new.fizz_buzz(50),   'App returns a string'
+
   end
   
 
 end
+
 
 
